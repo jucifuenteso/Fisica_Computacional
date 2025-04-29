a7b3765e49da01db090f796d7a3d4096d9ff45f9
jucifuenteso <jucifuenteso@unal.edu.co>
2025-04-29 08:52:18 -0500

    Proy

---
+++ Prueba/Prueba/Prueba.f90
@@ -0,0 +1,25 @@
+!  Prueba.f90 
+!
+!  FUNCTIONS:
+!  Prueba - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Prueba
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Prueba
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Prueba
+    print *, 'Hello World'
+
+    end program Prueba
+
